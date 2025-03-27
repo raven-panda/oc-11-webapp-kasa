933a4df99a3e0e5c039df2e7a517d9263ff3540d
raven-panda <ambroise.maire7@gmail.com>
2025-03-27 20:57:02 +0100

    add: added variant class to page title card

--- src/components/cards/page-title-card/PageTitleCard.jsx
+++ src/components/cards/page-title-card/PageTitleCard.jsx
@@ -2,11 +2,12 @@ import styles from "./PageTitleCard.module.scss";
 
 /**
  * @param title {string} The text to render inside the card
+ * @param variant {"home"|"about"|"housing"} The variant category
  * @returns {JSX.Element}
  */
-export default function PageTitleCard({ title }) {
+export default function PageTitleCard({ title, variant = "home" }) {
   return (
-    <section className={styles.page_title_card}>
+    <section className={styles.page_title_card + " " + (styles[variant] ?? styles.home)}>
       {title && <h1>{title}</h1>}
     </section>
   );
