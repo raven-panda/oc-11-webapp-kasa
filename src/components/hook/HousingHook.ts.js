b43f3de506b8a8ba964b5b69d017ae0641d88333
raven-panda <ambroise.maire7@gmail.com>
2025-03-27 20:36:04 +0100

    docs: added docs in housing hook

--- src/components/hook/HousingHook.ts.js
+++ src/components/hook/HousingHook.ts.js
@@ -1,6 +1,10 @@
 import { useEffect, useState } from "react";
 import housingsFixture from "../../__fixtures__/logements.json";
 
+/**
+ * Hook used for housings data retrieval
+ * @returns {{housings: any[]}}
+ */
 export default function useHousingData() {
   const [housings, setHousings] = useState([]);
 
