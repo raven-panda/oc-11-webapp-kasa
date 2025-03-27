98de9effb3256fad367d5e688826b81207670a48
raven-panda <ambroise.maire7@gmail.com>
2025-03-27 17:51:47 +0100

    edit: updated layout, created kasa logo component

--- src/components/layout/MainLayout.jsx
+++ src/components/layout/MainLayout.jsx
@@ -1,23 +1,28 @@
 import styles from './MainLayout.module.scss';
 import { Link } from "react-router-dom";
+import KasaLogo from "../../assets/icon/KasaLogo.jsx";
 
+/**
+ * @param children {ReactNode} Children to display in `<main>` element
+ * @returns {JSX.Element}
+ */
 export default function MainLayout({ children }) {
-  return <>
-    <header className={styles.header}>
-      <picture>
-        <source media="(max-width: 375px)" srcSet="/kasa-logo-146.png"/>
-        <img
-          src="/kasa-logo-211.png"
-          alt="Kasa Logo"
-        />
-      </picture>
-      <nav>
-        <Link to={"/"}>Accueil</Link>
-        <Link to={"/about"}>A Propos</Link>
-      </nav>
-    </header>
-    <main>
-      {children}
-    </main>
-  </>;
+  return <section>
+    <div className={styles.layout_main_container}>
+      <header className={styles.header}>
+        <KasaLogo className={styles.layout_logo} />
+        <nav>
+          <Link to={"/"}>Accueil</Link>
+          <Link to={"/about"}>A Propos</Link>
+        </nav>
+      </header>
+      <main>
+        {children}
+      </main>
+    </div>
+    <footer className={styles.layout_footer}>
+      <KasaLogo width={122} height={39} />
+      <p>&#169; 2020 Kasa. All rights reserved</p>
+    </footer>
+  </section>;
 }
